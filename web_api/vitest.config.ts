2b6f635989f4c7ff544a5e4e6718e3c834aee94d
Eaviyi <eavidy@qq.com>
2025-02-18 12:06:24 +0800

    fix: API 测试

--- web_api/vitest.config.ts
+++ web_api/vitest.config.ts
@@ -1,4 +1,5 @@
 import type { ViteUserConfig } from "vitest/config";
+import tsconfigPaths from "vite-tsconfig-paths";
 import process from "node:process";
 
 const PG_URL = process.env.PG_URL || "pg://test@127.0.0.1:5432/postgres";
@@ -10,7 +11,8 @@ export default {
       TEST_LOGIN_DB: PG_URL,
       IJIA_TEMPLATE_DBNAME: "test_ijia_template",
     },
-    setupFiles: ["../data/test/setup/extend_yoursql.ts"],
-    globalSetup: "../data/test/setup/setup_pgsql.ts",
+    setupFiles: ["../deps/ijia-data/test/setup/extend_yoursql.ts"],
+    globalSetup: "../deps/ijia-data/test/setup/setup_pgsql.ts",
   },
+  plugins: [tsconfigPaths({})],
 } satisfies ViteUserConfig;
