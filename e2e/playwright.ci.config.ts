4cc30daa2f0e469bb4c9c3122bd641c247693e95
Eaviyi <eavidy@qq.com>
2025-03-18 20:02:11 +0800

    fix:

--- e2e/playwright.ci.config.ts
+++ e2e/playwright.ci.config.ts
@@ -20,11 +20,13 @@ export default defineConfig({
         browserName: "firefox",
       },
     },
+    // CI 的 webkit 总是出现测试失败，暂时不使用
+    /* ,
     {
       name: "WebKit",
       use: {
         browserName: "webkit",
       },
-    },
+    }, */
   ],
 });
