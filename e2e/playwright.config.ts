d148cb230140a964e31458dbe770fe156a56c835
Eaviyi <eavidy@qq.com>
2025-03-29 20:05:45 +0800

    ci: 更改 e2e 测试数据库默认端口

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from "@playwright/test";
 import process from "process";
 export const env = {
-  WEB_URL: process.env.WEB_URL || "http://localhost:5173",
-  DATABASE_URL: process.env.DATABASE_URL || "pg://postgres@localhost:5432/ijia_test",
+  WEB_URL: process.env.WEB_URL || "http://localhost:9002",
+  DATABASE_URL: process.env.DATABASE_URL || "pg://postgres@localhost:5442/ijia",
 };
 
 export default defineConfig({
