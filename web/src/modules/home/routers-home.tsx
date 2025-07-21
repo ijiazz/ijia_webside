e77c88ab84c33cf7d886691d5856c740195714a0
Eaviyi <eavidy@qq.com>
2025-07-21 19:31:37 +0800

    fix: 优化样式

--- web/src/modules/home/routers-home.tsx
+++ web/src/modules/home/routers-home.tsx
@@ -5,13 +5,7 @@ import { HomePageRes } from "@/api.ts";
 
 export const page: LazyRoute = {
   Component: HomePage,
-  loader: async (): Promise<HomePageRes> => {
-    return api["/live/screen/home"].get().catch(
-      (res): HomePageRes => ({
-        god_user: { user_name: "" },
-        god_user_platforms: [],
-        current_user: { encountering_time: "", user_id: "", user_name: "" },
-      }),
-    );
+  loader: async (): Promise<HomePageRes | undefined> => {
+    return api["/live/screen/home"].get().catch((res) => undefined);
   },
 };
