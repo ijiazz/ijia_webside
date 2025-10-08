6976f5d52896299b52dfa4c28a5881041762ac1d
Eaviyi <eavidy@qq.com>
2025-10-08 19:41:46 +0800

    feat: 导航时显示 loading 状态

--- web/src/routes/__root.tsx
+++ web/src/routes/__root.tsx
@@ -2,21 +2,20 @@ import { createRootRoute, Outlet } from "@tanstack/react-router";
 
 import React, { useEffect } from "react";
 import { removeLoading } from "@/app.ts";
-import { PageLoading } from "@/common/page_state/Loading.tsx";
-import { NotFoundPage } from "@/common/page_state/NotFound.tsx";
 import { LayoutDirectionProvider } from "@/provider/LayoutDirectionProvider.tsx";
+import { RouterProgress } from "./-layout/Progress.tsx";
 
 export const Route = createRootRoute({
   component: () => {
     useEffect(() => {
       removeLoading();
     }, []);
+
     return (
       <LayoutDirectionProvider>
         <Outlet />
+        <RouterProgress />
       </LayoutDirectionProvider>
     );
   },
-  notFoundComponent: NotFoundPage,
-  pendingComponent: PageLoading,
 });
