0f9a15f8b8e6818f50bd97931a738218296a6a00
Eaviyi <eavidy@qq.com>
2025-11-11 21:37:23 +0800

    fix:

--- web_api/src/sql/user.ts
+++ web_api/src/sql/user.ts
@@ -2,7 +2,7 @@ import { HttpError } from "@/global/errors.ts";
 import { user } from "@ijia/data/db";
 import { v } from "./utils.ts";
 import { select } from "@asla/yoursql";
-import { dbPool } from "node_modules/@ijia/data/dist/dbclient.js";
+import { dbPool } from "@ijia/data/dbclient";
 
 /** 从数据库获取有效用户信息 */
 export async function getValidUserSampleInfoByUserId(userId: number) {
