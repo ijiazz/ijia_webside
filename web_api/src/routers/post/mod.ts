78957e5c72c43e28fbd12453a4c8f89db622e5ec
Eaviyi <eavidy@qq.com>
2025-11-30 21:47:19 +0800

    fix:

--- web_api/src/routers/post/mod.ts
+++ web_api/src/routers/post/mod.ts
@@ -1,11 +1,16 @@
 export { default } from "./_route.ts";
 export * from "./-api.ts";
+
 import "./.put.ts";
 import "./list.get.ts";
+import "./god_list.get.ts";
+
 import "./$postId/.patch.ts";
 import "./$postId/.delete.ts";
 import "./$postId/like.post.ts";
 import "./$postId/report.post.ts";
+
+import "./group/list.get.ts";
 
 import "./review/next.get.ts";
 import "./review/$reviewId/commit.post.ts";
