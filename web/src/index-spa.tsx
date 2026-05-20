14f48d245df9a3bd093fe973c8d25ba2c0484cb8
Eaviyi <eavidy@qq.com>
2026-05-20 14:02:27 +0800

    refactor: 移除不再使用的用户认证和懒加载组件，简化代码结构

--- web/src/index-spa.tsx
+++ web/src/index-spa.tsx
@@ -1,7 +1,5 @@
-import { loginByAccessToken } from "@/common/user.ts";
 import { createRoot } from "react-dom/client";
 import { router } from "./common/router.tsx";
-import { REQUEST_AUTH_KEY } from "./api.ts";
 import * as sentry from "@sentry/react";
 import "./index-client-entry.ts";
 import { RouterProvider } from "@tanstack/react-router";
@@ -14,15 +12,4 @@ const mountApp = () => {
     onRecoverableError: sentry.reactErrorHandler(),
   }).render(<RouterProvider router={router} />);
 };
-paseAccessToken();
 mountApp();
-
-function paseAccessToken() {
-  const url = new URL(location.href);
-  const access_token = url.searchParams.get(REQUEST_AUTH_KEY);
-  if (access_token) {
-    loginByAccessToken(access_token);
-    url.searchParams.delete(REQUEST_AUTH_KEY);
-    location.replace(url);
-  }
-}
