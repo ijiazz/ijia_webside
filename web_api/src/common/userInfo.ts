0a2a679e6550ff15afffe70017b10988c87f01a2
Eaviyi <eavidy@qq.com>
2026-05-23 18:29:24 +0800

    refactor: 重构 UserInfo 类，简化用户信息创建逻辑

--- web_api/src/common/userInfo.ts
+++ web_api/src/common/userInfo.ts
@@ -1,28 +1,33 @@
-import { createHttpUserInfo, ERRORS, HttpUserInfo } from "@ijia/school-db/auth";
+import { AccessUserData, ERRORS, HttpUserInfo, JWTAuth } from "@ijia/school-db/auth";
 import { HttpError, RequiredLoginError } from "@/common/errors.ts";
 import { verifyAccessToken } from "@/common/jwt.ts";
 
-export type UserInfo = HttpUserInfo;
+function createError({ code, message }: { code: string; message: string }) {
+  switch (code) {
+    case ERRORS.AccountFrozen:
+      return new HttpError(423, message);
+    case ERRORS.AccountNotExist:
+      return new HttpError(404, message);
 
+    case ERRORS.TokenExpired:
+    case ERRORS.RequiredLogin:
+    default:
+      return new RequiredLoginError(message);
+  }
+}
+export class UserInfo extends HttpUserInfo {
+  constructor(accessToken?: string) {
+    const jwtAuth = new JWTAuth({ accessToken, verifyAccessToken, createError });
+    super(jwtAuth, { createError, rootRoleId: Role.Root });
+    this.#jwtAuth = jwtAuth;
+  }
+  #jwtAuth: JWTAuth<AccessUserData>;
+  override checkUpdateToken(force = false) {
+    return this.#jwtAuth.checkUpdateToken(force);
+  }
+}
 export function createUserInfo(accessToken?: string): UserInfo {
-  return createHttpUserInfo({
-    verifyAccessToken,
-    accessToken,
-    createError: ({ code, message }) => {
-      switch (code) {
-        case ERRORS.AccountFrozen:
-          return new HttpError(423, message);
-        case ERRORS.AccountNotExist:
-          return new HttpError(404, message);
-
-        case ERRORS.TokenExpired:
-        case ERRORS.RequiredLogin:
-        default:
-          return new RequiredLoginError(message);
-      }
-    },
-    rootRoleId: Role.Root,
-  });
+  return new UserInfo(accessToken);
 }
 export enum Role {
   Root = "root",
