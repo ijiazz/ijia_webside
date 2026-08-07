c7a599cf67d0fa719d715b27fccb11a486e8edf3
Eaviyi <eavidy@qq.com>
2026-08-07 15:07:27 +0800

    feat: 重构数据库初始化逻辑，删除冗余文件，优化测试设置

--- web_api/test/setup/global_setup.ts
+++ web_api/test/setup/global_setup.ts
@@ -1,22 +1,9 @@
 import { TestProject } from "vitest/node";
 
-import { createInitIjiaDb } from "@ijia/school-db/testlib";
-import { DB_CONNECT_INFO } from "#test/utils/db.ts";
-import { DbManage } from "@asla/pg";
+import { initPublicDB } from "./utils/db.ts";
 
-const PUBLIC_DB_NAME = "test_ijia_public";
 export async function setup(project: TestProject) {
-  await using conn = await DbManage.connect(DB_CONNECT_INFO);
-  const client = conn.dbClient;
-  const [info] = await client.queryRows<{ connections: number }>(`
-    SELECT count(*)::INT AS connections
-    FROM pg_stat_activity WHERE datname = '${PUBLIC_DB_NAME}'`);
-  if (info.connections) {
-    console.log("跳过初始化公共数据库");
-    return;
-  }
-  console.log("初始化公共数据库");
-  await createInitIjiaDb(DB_CONNECT_INFO, PUBLIC_DB_NAME, { dropIfExists: true, test: true });
+  await initPublicDB();
 }
 
 export function teardown() {}
